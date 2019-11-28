da9147ba4e1a0ccc805618db3efd2f6fbbab3f27
Wesley Hatley <whatley@lenovo.com>
2019-11-27 21:21:09 -0500

    Updates

---
+++ precache-manifest.6554f09863523cc062e932e752fd7095.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f357c52a06ea6ea6a2bb5a45e869811",
+    "url": "/Material/index.html"
+  },
+  {
+    "revision": "27901e158c44044107ed",
+    "url": "/Material/static/css/2.d0176e96.chunk.css"
+  },
+  {
+    "revision": "7393b66afc1f6ac938c6",
+    "url": "/Material/static/css/main.b5921abd.chunk.css"
+  },
+  {
+    "revision": "27901e158c44044107ed",
+    "url": "/Material/static/js/2.e8445760.chunk.js"
+  },
+  {
+    "revision": "7393b66afc1f6ac938c6",
+    "url": "/Material/static/js/main.6ad4b386.chunk.js"
+  },
+  {
+    "revision": "28b86612cfc95e07cf29",
+    "url": "/Material/static/js/runtime~main.02c560c7.js"
+  },
+  {
+    "revision": "7a6bc10028a79f78e424a9c71499fa77",
+    "url": "/Material/static/media/avatar.7a6bc100.jpg"
+  },
+  {
+    "revision": "4d2f2f4baa617ba08685b4d9118dc029",
+    "url": "/Material/static/media/bg-pricing.4d2f2f4b.jpeg"
+  },
+  {
+    "revision": "6ee66b2cb8a3d9988e37b83b3b4a590e",
+    "url": "/Material/static/media/card-1.6ee66b2c.jpeg"
+  },
+  {
+    "revision": "ae10829f019b0103525a6cd1102ee213",
+    "url": "/Material/static/media/card-2.ae10829f.jpeg"
+  },
+  {
+    "revision": "4ae4eeff542ab32204e8409d170e14a0",
+    "url": "/Material/static/media/card-3.4ae4eeff.jpeg"
+  },
+  {
+    "revision": "3122abf4a3e1067926c08dee7684522d",
+    "url": "/Material/static/media/card-profile1-square.3122abf4.jpg"
+  },
+  {
+    "revision": "5b7c1cad66a2efdbf3604615cc9d7d1c",
+    "url": "/Material/static/media/clint-mckoy.5b7c1cad.jpg"
+  },
+  {
+    "revision": "ebe9884b2152a08a525fcd5054019532",
+    "url": "/Material/static/media/image_placeholder.ebe9884b.jpg"
+  },
+  {
+    "revision": "35f7d126c07a40a893f3f6cc8d1c3717",
+    "url": "/Material/static/media/lock.35f7d126.jpeg"
+  },
+  {
+    "revision": "59fc83e26c663ade5dcae6e7d56ca970",
+    "url": "/Material/static/media/login.59fc83e2.jpeg"
+  },
+  {
+    "revision": "a80cd7a77206b15ee13a582ea53b4cab",
+    "url": "/Material/static/media/logo-white.a80cd7a7.svg"
+  },
+  {
+    "revision": "aba54d652e5df3b9f1f8024cbeb6630f",
+    "url": "/Material/static/media/marc.aba54d65.jpg"
+  },
+  {
+    "revision": "1eb7fff2a469da3cc6c3311572d7696d",
+    "url": "/Material/static/media/placeholder.1eb7fff2.jpg"
+  },
+  {
+    "revision": "081bf019322fc100b3b84f34d9bff302",
+    "url": "/Material/static/media/product1.081bf019.jpg"
+  },
+  {
+    "revision": "a0e400118b382fc007111de984da7552",
+    "url": "/Material/static/media/product3.a0e40011.jpg"
+  },
+  {
+    "revision": "49b54380ba092ba68ab160f3dd8d5773",
+    "url": "/Material/static/media/register.49b54380.jpeg"
+  },
+  {
+    "revision": "61c4c50e61d68de31c8951af5470a2c2",
+    "url": "/Material/static/media/sidebar-1.61c4c50e.jpg"
+  },
+  {
+    "revision": "d30c9e30978c9278180dcce2bb66c054",
+    "url": "/Material/static/media/sidebar-2.d30c9e30.jpg"
+  },
+  {
+    "revision": "c963330bcd48670a75ffdd38622b1532",
+    "url": "/Material/static/media/sidebar-3.c963330b.jpg"
+  },
+  {
+    "revision": "f34e943fbffb73c68e761828b1c00980",
+    "url": "/Material/static/media/sidebar-4.f34e943f.jpg"
+  }
+]);
